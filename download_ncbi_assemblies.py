902aca2e61ca655dd3e3cfb5393b1ceae19c5779
rfm <rfm.1991.cas@gmail.com>
2020-07-04 13:30:13 +0100

    Updated download_ncbi_assemblies.py to work with latest Genome Assembly and Annotation report table format.

--- download_ncbi_assemblies.py
+++ download_ncbi_assemblies.py
@@ -63,14 +63,14 @@ def main(input_table, output_directory):
 
     # open table downloaded from NCBI
     with open(input_table, 'r') as table:
-        lines = list(csv.reader(table, delimiter='\t'))
+        lines = list(csv.reader(table, delimiter=','))
 
     # get urls for samples that have refseq ftp path
-    refseq_urls = [line[17] for line in lines[1:] if line[17] != '-']
+    refseq_urls = [line[15] for line in lines[1:] if line[15] != '-']
     refseq_assemblies_ids = [url.split('/')[-1] for url in refseq_urls]
 
     # try to get genbank urls for samples that had no refseq ftp path
-    genbank_urls = [line[18] for line in lines[1:] if line[17] == '-' and line[18] != '-']
+    genbank_urls = [line[16] for line in lines[1:] if line[15] == '-' and line[17] != '-']
     genbank_assemblies_ids = [url.split('/')[-1] for url in genbank_urls]
 
     urls = refseq_urls + genbank_urls
@@ -78,13 +78,15 @@ def main(input_table, output_directory):
 
     ftp_urls = []
     for url in range(len(urls)):
-        ftp_url = '{0}/{1}_genomic.fna.gz'.format(urls[url], assemblies_ids[url])
+        ftp_url = '{0}/{1}_genomic.fna.gz'.format(urls[url],
+                                                  assemblies_ids[url])
         ftp_urls.append(ftp_url)
 
     files_number = len(ftp_urls)
 
     assemblies_ids = ['{0}.fasta.gz'.format(url) for url in assemblies_ids]
-    assemblies_ids = [os.path.join(output_directory, file_name) for file_name in assemblies_ids]
+    assemblies_ids = [os.path.join(output_directory, file_name)
+                      for file_name in assemblies_ids]
 
     print('\nStarting download of {0} fasta.gz files...'.format(len(ftp_urls)))
     start = time.time()
@@ -102,8 +104,10 @@ def main(input_table, output_directory):
     delta = end - start
     minutes = int(delta/60)
     seconds = delta % 60
-    print('Finished downloading {0}/{1} fasta.gz files.\nElapsed Time: {2}m{3:.0f}s'.format(files_number-failures_number, files_number,
-                                                                                            minutes, seconds))
+    print('Finished downloading {0}/{1} fasta.gz files.'
+          '\nElapsed Time: {2}m{3:.0f}s'
+          ''.format(files_number-failures_number,
+                    files_number, minutes, seconds))
 
 
 def parse_arguments():
@@ -111,11 +115,15 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument('-t', '--input_table', type=str, required=True, dest='input_table',
-                        help='TSV file downloaded from the NCBI Genome Assembly and Annotation report.')
+    parser.add_argument('-t', '--input_table', type=str,
+                        required=True, dest='input_table',
+                        help='TSV file downloaded from the NCBI '
+                             'Genome Assembly and Annotation report.')
 
-    parser.add_argument('-o', '--output_directory', type=str, required=True, dest='output_directory',
-                        help='Path to the directory where downloaded files will be stored.')
+    parser.add_argument('-o', '--output_directory', type=str,
+                        required=True, dest='output_directory',
+                        help='Path to the directory where downloaded '
+                             'files will be stored.')
 
     args = parser.parse_args()
 
